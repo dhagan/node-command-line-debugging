4bca0f58c3f2ee189769d61fe5e38ddb0a3f7e11
Douglas Hagan <dhagan@yahoo.com>
2013-10-14 22:36:08 -1000

    digging through xml

--- node/helloWorld.js
+++ node/helloWorld.js
@@ -1,23 +1,25 @@
 console.log("hello");
 
 var fs = require('fs'),
-  // eyes = require('eyes'),
-   util = require('util'),
+// eyes = require('eyes'),
+    util = require('util'),
     xml2js = require('xml2js');
 
 var parser = new xml2js.Parser();
 
-parser.on('end', function(result) {
-  //eyes.inspect(result);
-  console.log(util.inspect(result));
+parser.on('end', function (result) {
+    //eyes.inspect(result);
+    console.log(util.inspect(result));
+    var trk = result['gpx']['trk'];
+    console.log(result['gpx']['trk'][0]['trkseg'][0]['trkpt']);
 });
 
-fs.readFile(__dirname + '/foo.xml', function(err, data) {
-  parser.parseString(data);
+fs.readFile(__dirname + '/eight_ball.gpx', function (err, data) {
+    var xmlDoc = parser.parseString(data);
 });
 
 fs.writeFile('message.txt', 'Hello Node', function (err) {
-  if (err) throw err;
-  console.log('It\'s saved!');
+    if (err) throw err;
+    console.log('It\'s saved!');
 });
 
